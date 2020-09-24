96f1b7dd7d95c5fa31752eb18830cdd5ad3c7b07
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-09-24 19:33:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,11 +9,14 @@ end
 
 introduction('Jordan')
 
+
+
+
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am leearning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
-introduction('Jordan', 'Ruby')
+introduction_with_language('Jordan', 'Ruby')
 
 
 
